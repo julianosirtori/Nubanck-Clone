6307e47ea0f87566e2f25fe9083b2d58bbec472c
Juliano Sirtori <julianosirtori@gmail.com>
2020-04-25 14:33:44 -0300

    Criado Component Menu

---
+++ src/components/Menu/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import QRCode from 'react-native-qrcode-svg';
+
+import {
+  Container,
+  Code,
+  Nav,
+  NavItem,
+  NavText,
+  SignOutButton,
+  SignOutButtonText,
+} from './styles';
+
+export default function Menu() {
+  return (
+    <Container>
+      <Code>
+        <QRCode
+          value="teste"
+          size={80}
+          backgroundColor="#fff"
+          color="#8b10ae"
+        />
+      </Code>
+      <Nav>
+        <NavItem>
+          <Icon name="help-outline" size={20} color="#FFF" />
+          <NavText>Me ajuda</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="person-outline" size={20} color="#FFF" />
+          <NavText>Perfil</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="credit-card" size={20} color="#FFF" />
+          <NavText>Configurar cartão</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="smartphone" size={20} color="#FFF" />
+          <NavText>Configuração</NavText>
+        </NavItem>
+      </Nav>
+
+      <SignOutButton onPress={() => {}}>
+        <SignOutButtonText>Sair do App</SignOutButtonText>
+      </SignOutButton>
+    </Container>
+  );
+}
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,46 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
+import Menu from '~/components/Menu';
 
-import {Container} from './styles';
+import {
+  Container,
+  Content,
+  Card,
+  Title,
+  Description,
+  Annotation,
+  CardFooter,
+  CardHeader,
+  CardContent,
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Menu />
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 197.611,65</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferencia de R$ 32,00 recebida de Juliano Sirtori as 6:00h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
